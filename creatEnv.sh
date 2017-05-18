bca3559ffd9db6902b720d56dbfc085fc69b6a93
luxiaolei <26442779@qq.com>
2017-05-18 13:53:27 +0800

    add pip tf11

--- creatEnv.sh
+++ creatEnv.sh
@@ -4,13 +4,8 @@ conda env create -f env_py35_tf101.yml;
 conda env create -f env_py36_tf101.yml;
 
 conda env create -f env_py35_tf11_np111.yml; 
-source activate env_py35_tf11_np111;
-pip install tensorflow_gpu-1.1.0-cp35-cp35m-linux_x86_64.whl;
-source deactivate;
 
+conda env create -f env_py35_tf11_np112.yml;
 
-conda env create -f env_py35_tf11_np112.yml; 
-source activate env_py35_tf11_np112;
-pip install tensorflow_gpu-1.1.0-cp35-cp35m-linux_x86_64.whl;
-source deactivate;
+conda env create -f env_py36_tf11_np112.yml
 
